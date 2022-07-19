5bea77722c2d33bee4287c429401f4bec99304f9
StevenUniverseAguirre <99989825+StevenUniverseAguirre@users.noreply.github.com>
2022-07-19 08:39:30 -0600

    Update ordenacion.js

--- Guia2/ejercicio3/js/ordenacion.js
+++ Guia2/ejercicio3/js/ordenacion.js
@@ -1,78 +1,89 @@
 var ascendentes = new Array();
-var max, contenido="", temp;
-function voto(){
-    do{
+var max, contenido = "", temp;
+function voto() {
+    do {
         max = prompt("Ingrese la cantidad de numeros a ordenar");
-        if(isNaN(max)){
+        if (isNaN(max)) {
             alert("No es número");
         }
         continue;
-    
-    }while(max<2 || isNaN(max));
+
+    } while (max < 2 || isNaN(max));
 }
-var eleccion =  prompt("¿De que forma desea ver el orden?, ascendente o descendente","");
-switch(eleccion){
+var eleccion = prompt("¿De que forma desea ver el orden?, ascendente o descendente", "");
+switch (eleccion) {
     case "ascendente":
     case "Ascendente":
         voto();
-        for(i=0; i<max; i++){
+        for (i = 0; i < max; i++) {
             ascendentes[i] = parseInt(prompt("Número " + (parseInt(i) + 1), ""));
         }
-            var dato = document.getElementById('datos');
-            with(document){
-                contenido += "<h1>Listado de números</h1>";
-                for(i=0; i<max; i++){
-                    contenido += " , " + ascendentes[i];
-                }
-                for(i=0; i<max-1; i++){
-                    for(j=i+1; j<max; j++){
-                        if(ascendentes[i]>ascendentes[j]){
-                            temp = ascendentes[j];
-                            ascendentes[j] = ascendentes[i];
-                            ascendentes[i] = temp;
-                        }
+        var dato = document.getElementById('datos');
+        with (document) {
+            contenido += "<h1>Listado de números</h1>";
+            contenido += "<table>\n\t<tbody>\n\t\t<tr>\n";
+
+            for (i = 0; i < max; i++) {
+                contenido += "\t\t\t<td>" + ascendentes[i] + "</td>\n";
+            }
+            contenido += "<table>\n\t<tbody>\n\t\t<tr>\n";
+            for (i = 0; i < max - 1; i++) {
+                for (j = i + 1; j < max; j++) {
+                    if (ascendentes[i] > ascendentes[j]) {
+                        temp = ascendentes[j];
+                        ascendentes[j] = ascendentes[i];
+                        ascendentes[i] = temp;
                     }
                 }
-                contenido += "<h1>Listado de números ascendentes:</h1>"
-                for(i=0; i<max; i++){
-                    contenido += " , " + ascendentes[i];
-                }
             }
-            
-        
+            contenido += "<h1>Listado de números ascendentes:</h1>\n";
+
+
+            for (i = 0; i < max; i++) {
+                contenido += "\t\t\t<td>" + ascendentes[i] + "</td>\n";
+            }
+            contenido += "\t\t</tr>\n\t</tbody>\n</table>\n<br />\n\n";
+        }
+
+
         dato.innerHTML = contenido;
-        
-    break;
+
+        break;
     case "descendente":
     case "Descendente":
         voto();
-        for(i=0; i<max; i++){
+        for (i = 0; i < max; i++) {
             ascendentes[i] = parseInt(prompt("Número " + (parseInt(i) + 1), ""));
         }
-            var dato = document.getElementById('datos');
-            with(document){
-                contenido += "<h1>Listado de números</h1>";
-                for(i=0; i<max; i++){
-                    contenido += " , " + ascendentes[i];
-                }
-                for(i=0; i<max-1; i++){
-                    for(j=i+1; j<max; j++){
-                        if(ascendentes[i]<ascendentes[j]){
-                            temp = ascendentes[j];
-                            ascendentes[j] = ascendentes[i];
-                            ascendentes[i] = temp;
-                        }
+        var dato = document.getElementById('datos');
+        with (document) {
+            contenido += "<h1>Listado de números</h1>";
+            contenido += "<table>\n\t<tbody>\n\t\t<tr>\n";
+            for (i = 0; i < max; i++) {
+                contenido += "\t\t\t<td>" + ascendentes[i] + "</td>\n";
+            }
+            contenido += "<table>\n\t<tbody>\n\t\t<tr>\n";
+            for (i = 0; i < max - 1; i++) {
+                for (j = i + 1; j < max; j++) {
+                    if (ascendentes[i] < ascendentes[j]) {
+                        temp = ascendentes[j];
+                        ascendentes[j] = ascendentes[i];
+                        ascendentes[i] = temp;
                     }
                 }
-                contenido += "<h1>Listado de números descendentes:</h1>"
-                for(i=0; i<max; i++){
-                    contenido += " , " + ascendentes[i];
-                }
             }
-            
-        
+            contenido += "<h1>Listado de números descendentes:</h1>";
+
+            contenido += "\t\t</tr>\n\t</tbody>\n</table>\n<br />\n\n";
+            for (i = 0; i < max; i++) {
+                contenido += "\t\t\t<td>" + ascendentes[i] + "</td>\n";
+            }
+            contenido += "\t\t</tr>\n\t</tbody>\n</table>\n<br />\n\n";
+        }
+
+
         dato.innerHTML = contenido;
-    break;
+        break;
     default:
         document.write("Error, datos invalidos");
 }
